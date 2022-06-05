ba33b8cf6cc9cf648a7a3eee7872dfe97a64696d
Sean Mealin <spmealin@gmail.com>
2022-06-05 00:37:58 -0400

    feat: adds the ability to destroy all of the children or only one child of a GameObject

--- src/models/GameObject.ts
+++ src/models/GameObject.ts
@@ -7,6 +7,27 @@ export abstract class GameObject implements Updatable {
     protected _children: GameObject[] = [];
 
     /**
+     * Called when this object should be destroied.
+     */
+    destroy(): void {
+        this._children.forEach((child) => child.destroy());
+        this._children = [];
+    }
+
+    /**
+     * Destroy a specific GameObject.
+     *
+     * @param child - the GameObject to destroy
+     */
+    destroyChild(child: GameObject): void {
+        const index = this._children.indexOf(child);
+        if (index !== -1) {
+            child.destroy();
+            this._children.splice(index, 1);
+        }
+    }
+
+    /**
      * Update this and all of the children.
      *
      * @param delta - the time in milliseconds since the last update
